a30299e3d528c1da4c7f77f53d67ab1151552440
Pekka Okkonen <pekka.okkonen1@gmail.com>
2019-05-08 15:10:30 +0300

    changes minutes input to buttons

--- js/main.js
+++ js/main.js
@@ -4,22 +4,19 @@ window.onload=function() {
     let startDate = document.getElementById("startDate");
     let endDate = document.getElementById("endDate");
     let lastN = document.getElementById("lastN");
-    let minutes = document.getElementById("minutes");
-    let date1, date2, nValue, minutes2;
+    let date1, date2, nValue;
 
     startDate.addEventListener("change", function() {date1 = startDate.value});
     endDate.addEventListener("change", function() {date2 = endDate.value});
     lastN.addEventListener("change", function() {nValue = lastN.value});
-    minutes.addEventListener("change", function() {minutes2 = minutes.value});
 
     document.querySelector("#btnTimePer").addEventListener("click", function() {
         let datesList = [];
-        datesList.push(date1)
-        datesList.push(date2)
+        datesList.push(date1);
+        datesList.push(date2);
 
         document.getElementById("headTimePer").style.color = "green";
         document.getElementById("headLastN").style.color = "black";
-        document.getElementById("headMinutes").style.color = "black";
 
         MashupPlatform.wiring.pushEvent("Dates", datesList);
     })
@@ -27,15 +24,27 @@ window.onload=function() {
     document.querySelector("#btnLastN").addEventListener("click", function() {
         document.getElementById("headLastN").style.color = "green";
         document.getElementById("headTimePer").style.color = "black";
-        document.getElementById("headMinutes").style.color = "black";
 
         MashupPlatform.wiring.pushEvent("nValue", nValue);
     })
 
-    document.querySelector("#btnMinutes").addEventListener("click", function() {
-        document.getElementById("headMinutes").style.color = "green";
-
-        MashupPlatform.wiring.pushEvent("Minutes", minutes2);
+    document.querySelector("#btn10").addEventListener("click", function() {
+        document.getElementById("btn10").style.color = "green";
+        document.getElementById("btn20").style.color = "black";
+        document.getElementById("btn30").style.color = "black";
+        MashupPlatform.wiring.pushEvent("Minutes", "10");
+    })
+    document.querySelector("#btn20").addEventListener("click", function() {
+        document.getElementById("btn20").style.color = "green";
+        document.getElementById("btn10").style.color = "black";
+        document.getElementById("btn30").style.color = "black";
+        MashupPlatform.wiring.pushEvent("Minutes", "20");
+    })
+    document.querySelector("#btn30").addEventListener("click", function() {
+        document.getElementById("btn30").style.color = "green";
+        document.getElementById("btn20").style.color = "black";
+        document.getElementById("btn10").style.color = "black";
+        MashupPlatform.wiring.pushEvent("Minutes", "30");
     })
 
 }
