eb76a2706dda48bed9cfbe36b38aa9f31af4cf2d
Pekka Okkonen <pekka.okkonen1@gmail.com>
2019-05-08 14:13:01 +0300

    added minutes input

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ window.onload=function() {
     let startDate = document.getElementById("startDate");
     let endDate = document.getElementById("endDate");
     let lastN = document.getElementById("lastN");
-    let date1, date2, nValue;
+    let minutes = document.getElementById("minutes");
+    let date1, date2, nValue, minutes2;
 
-    startDate.addEventListener("change", function() {date1 = startDate.value})
-    endDate.addEventListener("change", function() {date2 = endDate.value})
-    lastN.addEventListener("change", function() {nValue = lastN.value})
+    startDate.addEventListener("change", function() {date1 = startDate.value});
+    endDate.addEventListener("change", function() {date2 = endDate.value});
+    lastN.addEventListener("change", function() {nValue = lastN.value});
+    minutes.addEventListener("change", function() {minutes2 = minutes.value});
 
     document.querySelector("#btnTimePer").addEventListener("click", function() {
         let datesList = [];
@@ -17,6 +19,7 @@ window.onload=function() {
 
         document.getElementById("headTimePer").style.color = "green";
         document.getElementById("headLastN").style.color = "black";
+        document.getElementById("headMinutes").style.color = "black";
 
         MashupPlatform.wiring.pushEvent("Dates", datesList);
     })
@@ -24,8 +27,15 @@ window.onload=function() {
     document.querySelector("#btnLastN").addEventListener("click", function() {
         document.getElementById("headLastN").style.color = "green";
         document.getElementById("headTimePer").style.color = "black";
+        document.getElementById("headMinutes").style.color = "black";
 
         MashupPlatform.wiring.pushEvent("nValue", nValue);
+    })
+
+    document.querySelector("#btnMinutes").addEventListener("click", function() {
+        document.getElementById("headMinutes").style.color = "green";
+
+        MashupPlatform.wiring.pushEvent("Minutes", minutes2);
     })
 
 }
